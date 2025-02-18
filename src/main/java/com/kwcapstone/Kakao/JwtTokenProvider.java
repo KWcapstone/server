b142115cb754606f6a8cbbfcc84b406482b95a6e
seoyeon0103 <angel6462@naver.com>
2025-02-18 15:54:48 +0900

    Feat: JwtTokenProvider 생성하기

--- src/main/java/com/kwcapstone/Kakao/JwtTokenProvider.java
+++ src/main/java/com/kwcapstone/Kakao/JwtTokenProvider.java
@@ -1,9 +1,24 @@
 package com.kwcapstone.Kakao;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.security.auth.message.AuthException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.ZonedDateTime;
+import java.util.Collections;
+import java.util.Date;
 
 //Refresh Token 넣고 다시 accessToken 하고 refreshToken 발급받을 때 필요한 함수들
 @Component
@@ -22,6 +37,102 @@ public class JwtTokenProvider {
 
     //생성자
     public JwtTokenProvider(
-            @Value("")
-    )
+            @Value("${jwt.secret-key}") final String secretKey,
+            @Value("${jwt.access-token-validity}") final long aTValidityMilliseconds,
+            @Value("${jwt.refresh-token-validity}") final long rTValidityMilliseconds
+    ){
+        //UTF-8 인코딩을 사용해서 문자열을 인코딩하고 HMAC-SHA 알고리즘으로 Key객체를 생성하는거임
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        this.aTValidityMilliseconds = aTValidityMilliseconds;
+        this.rTValidityMilliseconds = rTValidityMilliseconds;
+    }
+
+    //token 추출
+    public String extractToken(final HttpServletRequest request) {
+        String authorizationHeader = request.getHeader(HEADER_STRING);
+
+        if (authorizationHeader != null ||
+                authorizationHeader.startsWith(HEADER_STRING_PREFIX)) {
+            return authorizationHeader.substring(7);
+        }
+        throw new ResponseStatusException(
+                HttpStatus.UNAUTHORIZED, "Auth_001 : 토큰추출에 실패하였습니다.");
+    }
+
+    //Token 생성
+    public String createAccessToken(ObjectId memberId){
+        return createToken(memberId, aTValidityMilliseconds);
+    }
+
+    public String createRefreshToken(ObjectId memberId){
+        return createToken(memberId,rTValidityMilliseconds);
+    }
+
+    private String createToken(ObjectId memberId, Long validityMilliseconds){
+        //Jwt에 사용자 정보를 저장하기 위해 필요한 것
+        Claims claims = Jwts.claims();
+        claims.put("memberId", memberId.toString());
+
+        //현재시간 가져오기
+        ZonedDateTime now = ZonedDateTime.now();
+        //현재시간을 통해 jwt 발급 시점을 기록하기
+        ZonedDateTime tokenValidity
+                = now.plusSeconds(validityMilliseconds / 1000);
+
+        return Jwts.builder()
+                .setClaims(claims) //사용자 정보 설정
+                .setIssuedAt(Date.from(now.toInstant())) //발급 시간(iat) 설정
+                .setExpiration(Date.from(tokenValidity.toInstant())) //만료 시간 설정
+                .signWith(secretKey, SignatureAlgorithm.HS256) //서명 추가
+                .compact(); //최종 Jwt 샐성
+    }
+
+    //Jwt 에서 사용자 Id 추출
+    public Long getId(String token) {
+        return getClaims(token).getBody().get("id", Long.class);
+    }
+
+    //토큰 확인
+    public boolean isTokenValid(String token) {
+        try {
+            //Jwt의 사용자 정보 가져오기
+            Jws<Claims> claims = getClaims(token);
+            //만료 시간 가져오기
+            Date expiredDate = claims.getBody().getExpiration();
+            //현재 날짜 가져오기
+            Date now = new Date();
+
+            //만료 시간이 현재 이후인지 아닌지 확인하는 것(이후면 true)
+            return expiredDate.after(now);
+        } catch (ExpiredJwtException e) {
+            //jwt 만료되었을 때 발생하는 예외
+            throw new ResponseStatusException(
+                    HttpStatus.UNAUTHORIZED,
+                    "AUTH_002 : 토큰이 만료되었습니다.");
+        } catch (SecurityException e) {
+            //Jwt의 서명이 손상되었거나 위변조 된 경우
+            throw new ResponseStatusException(
+                    HttpStatus.UNAUTHORIZED,
+                    "AUTH_003 : 토큰의 서명이 손상되었거나 위변조 되었습니다.");
+        } catch (MalformedJwtException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.UNAUTHORIZED,
+                    "AUTH_004 : Jwt 형식이 올바르지 않습니다.");
+        } catch (UnsupportedJwtException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.UNAUTHORIZED,
+                    "AUTH_005 : 지원되지 않은 JWT 형식입니다.");
+        } catch (IllegalArgumentException e){
+            throw new ResponseStatusException(
+                    HttpStatus.UNAUTHORIZED,
+                    "AUTH_006 : Jwt가 null이거나 공백 문자열인 경우");
+        }
+    }
+
+    //TOKEN으로 JWT에서 사용자 정보 가져오기
+    private Jws<Claims> getClaims(String token) {
+        return Jwts.parserBuilder() //파서 객체 생성
+                .setSigningKey(secretKey) //서명 검증을 위한 secretKey 설정
+                .build().parseClaimsJws(token); //Jwt 파싱해서 사용자 정보 추출 및 검증 수행
+    }
 }
