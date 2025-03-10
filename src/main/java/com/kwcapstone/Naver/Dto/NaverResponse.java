306e0ef1573ccbb662b62f245733d23e28176e11
seoyeon0103 <angel6462@naver.com>
2025-03-11 00:49:27 +0900

    Feat: NaverResponse 추가

--- src/main/java/com/kwcapstone/Naver/Dto/NaverResponse.java
+++ src/main/java/com/kwcapstone/Naver/Dto/NaverResponse.java
@@ -1,4 +1,21 @@
 package com.kwcapstone.Naver.Dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+
 public class NaverResponse {
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class NaverLoginResponse{
+        private String id;
+        private String accessToken;
+
+        public NaverLoginResponse(ObjectId id, String accessToken){
+            this.id = id.toHexString();
+            this.accessToken = accessToken;
+        }
+    }
 }
