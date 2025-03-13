49c84f7c2428c084629d3d7c724fe098d9841505
seoyeon0103 <angel6462@naver.com>
2025-03-13 18:07:44 +0900

    Feat: NaverController 추가

--- src/main/java/com/kwcapstone/Naver/Controller/NaverController.java
+++ src/main/java/com/kwcapstone/Naver/Controller/NaverController.java
@@ -1,6 +1,7 @@
 package com.kwcapstone.Naver.Controller;
 
 import com.kwcapstone.Common.BaseResponse;
+import com.kwcapstone.Common.code.SuccessStatus;
 import com.kwcapstone.Naver.Dto.NaverResponse;
 import com.kwcapstone.Naver.Service.NaverService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -19,6 +20,6 @@ public class NaverController {
     @Operation(summary = "네이버 로그인 Api")
     @GetMapping("/naver")
     public BaseResponse<NaverResponse.NaverLoginResponse> naverLogin(@RequestParam("code")String code){
-        return BaseResponse.res(Succ)
+        return BaseResponse.res(SuccessStatus.USER_NAVER_LOGIN, naverService.naverLogin(code));
     }
 }
