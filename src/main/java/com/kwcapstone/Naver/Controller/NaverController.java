6715ef52db6b57e63b715edc545e051a5f3b2cf3
seoyeon0103 <angel6462@naver.com>
2025-03-13 18:07:04 +0900

    Feat: SuccessStatus에 소셜 로그인 성고 관련 code 추가

--- src/main/java/com/kwcapstone/Naver/Controller/NaverController.java
+++ src/main/java/com/kwcapstone/Naver/Controller/NaverController.java
@@ -1,4 +1,24 @@
 package com.kwcapstone.Naver.Controller;
 
+import com.kwcapstone.Common.BaseResponse;
+import com.kwcapstone.Naver.Dto.NaverResponse;
+import com.kwcapstone.Naver.Service.NaverService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth/login")
 public class NaverController {
+    private final NaverService naverService;
+
+    @Operation(summary = "네이버 로그인 Api")
+    @GetMapping("/naver")
+    public BaseResponse<NaverResponse.NaverLoginResponse> naverLogin(@RequestParam("code")String code){
+        return BaseResponse.res(Succ)
+    }
 }
