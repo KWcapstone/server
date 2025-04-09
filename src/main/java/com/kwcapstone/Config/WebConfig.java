94f144b163c7b8e3c83cbc33a281e86494821725
seyeon22 <sywolleych@gmail.com>
2025-04-09 14:52:25 +0900

    Refactor: 프론트 로컬 배포주소 추가

--- src/main/java/com/kwcapstone/Config/WebConfig.java
+++ src/main/java/com/kwcapstone/Config/WebConfig.java
@@ -6,7 +6,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    String[] accessURL = { "http://localhost:8080", "https://localhost:8080"};
+    String[] accessURL = { "http://localhost:8080", "https://localhost:8080",
+                            "http://localhost:5173", "https://localhost:5173" };
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
