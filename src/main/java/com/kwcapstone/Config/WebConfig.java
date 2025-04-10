5212e64297c7aa389a194e544115d107df7a451c
seoyeon0103 <angel6462@naver.com>
2025-04-10 14:13:00 +0900

    Refactor: 프론트.백엔드 배포주소 추가

--- src/main/java/com/kwcapstone/Config/WebConfig.java
+++ src/main/java/com/kwcapstone/Config/WebConfig.java
@@ -7,7 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     String[] accessURL = { "http://localhost:8080", "https://localhost:8080",
-                            "http://localhost:5173", "https://localhost:5173" };
+                            "http://localhost:5173", "https://localhost:5173",
+                            "https://moaba.vercel.app", "http://3.39.11.169:8080",
+                            "https://3.39.11.169:8080"};
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
