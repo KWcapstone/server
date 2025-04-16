037290b9f61aaa4c34632c3b8a505ecd73b37b09
seyeon22 <sywolleych@gmail.com>
2025-04-16 15:01:44 +0900

    Refactor: 서버 도메인 추가

--- src/main/java/com/kwcapstone/Config/WebConfig.java
+++ src/main/java/com/kwcapstone/Config/WebConfig.java
@@ -8,14 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     String[] accessURL = { "http://localhost:8080", "https://localhost:8080",
                             "http://localhost:5173", "https://localhost:5173",
-                            "https://moaba.vercel.app", "http://3.39.11.168:8080",
-                            "https://3.39.11.168:8080"};
+                            "https://moaba.vercel.app", "https://www.moaba.site",
+                            "http://3.39.11.168:8080", "https://3.39.11.168:8080"};
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(accessURL)
-                //.allowedMethods(accessURL)
                 .allowedMethods("GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS")
                 .allowedHeaders("*")
                 .maxAge(3000);
