e5699ccf66f74e585ebe940d9d2f8637cd46273e
seyeon22 <sywolleych@gmail.com>
2025-04-30 02:27:12 +0900

    Refactor: WebConfig 수정

--- src/main/java/com/kwcapstone/Config/WebConfig.java
+++ src/main/java/com/kwcapstone/Config/WebConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     String[] accessURL = { "http://localhost:8080", "https://localhost:8080",
-                            "http://localhost:5173", "https://localhost:5173",
                             "https://moaba.vercel.app", "https://www.moaba.site",
                             "https://moaba.site",
                             "http://3.39.11.168:8080", "https://3.39.11.168:8080",
@@ -24,8 +23,8 @@ public class WebConfig implements WebMvcConfigurer {
                         .allowedHeaders("*")
                         .allowedMethods("GET", "POST", "PATCH", "DELETE", "HEAD", "PUT", "OPTIONS")
                         .exposedHeaders("Content-Type", "Authorization")
-                        .maxAge(3000);
-                        //.allowCredentials(true);
+                        .maxAge(3000)
+                        .allowCredentials(true);
             }
         };
     }
