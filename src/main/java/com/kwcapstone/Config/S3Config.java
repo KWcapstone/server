fd274c0183acb4448eba64bf8d1c0b844b35d38e
seyeon22 <sywolleych@gmail.com>
2025-05-11 17:37:48 +0900

    Feat: 스크립트 중간 저장 구현

--- src/main/java/com/kwcapstone/Config/S3Config.java
+++ src/main/java/com/kwcapstone/Config/S3Config.java
@@ -1,43 +1,29 @@
 package com.kwcapstone.Config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-
-import java.io.File;
 
 @Configuration
 @RequiredArgsConstructor
 public class S3Config {
-    private final Environment env;
+    @Value("${aws.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${aws.secretAccessKey}")
+    private String secretAccessKey;
+
     @Bean
-    public S3Client defaultS3Client() {
-        return S3Client.builder().region(Region.AP_NORTHEAST_2).build();
-    }
-
-    // 업로드
-    public void uploadFileToS3(String s3Path, File file) {
-        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                .bucket(dataBucketName)
-                .key(s3Path)
-                .acl(ObjectCannedACL.BUCKET_OWNER_FULL_CONTROL)
+    public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKeyId, secretAccessKey);
+        return S3Client.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
-
-    }
-
-    // 다운로드
-    public File getS3Data(String s3Path, String fileName) {
-        GetObjectRequest objectRequest = GetObjectRequest.builder()
-                .bucket(dataBucketName)
-                .key(s3Path)
-                .build();
-
-
     }
 }
--- src/main/java/com/kwcapstone/Service/ConferenceService.java
+++ src/main/java/com/kwcapstone/Service/ConferenceService.java
@@ -7,9 +7,14 @@ import com.kwcapstone.Repository.ProjectRepository;
 import com.kwcapstone.Security.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 @Service
@@ -18,6 +23,7 @@ import java.time.LocalDateTime;
 public class ConferenceService {
 
     private final ProjectRepository projectRepository;
+    private final S3Service s3Service;
 
     public NewProjectResponseDto projectCreate(PrincipalDetails principalDetails) {
         ObjectId memberId = principalDetails.getId();
@@ -51,7 +57,43 @@ public class ConferenceService {
         return responseDto;
     }
 
-//    public Void scriptSave(PrincipalDetails principalDetails, ScriptMessageRequestDto requestDto) {
-//
-//    }
+    public Void scriptSave(PrincipalDetails principalDetails, ScriptMessageRequestDto requestDto) {
+        ObjectId memberId = principalDetails.getId();
+        try {
+            String projectIdStr = requestDto.getProjectId();
+            ObjectId projectId = new ObjectId(projectIdStr);
+
+            Project project = projectRepository.findByProjectId(projectId);
+            if (project == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "프로젝트를 찾을 수 없습니다.");
+            }
+            if (!project.getCreator().equals(memberId)) {
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN, "해당 프로젝트 대한 권한이 없습니다.");
+            }
+
+            String content = requestDto.getScription();
+
+            // 임시 디렉토리 경로 확인 및 생성
+            String tmpDirPath = System.getProperty("java.io.tmpdir");
+            File tmpDir = new File(tmpDirPath);
+            if (!tmpDir.exists() && !tmpDir.mkdirs()) {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "임시 폴더 생성에 실패했습니다.");
+            }
+
+            // 임시 파일에 저장 (append 모드)
+            String fileName = "script_" + projectId + ".txt";
+            File file = new File(System.getProperty("java.io.tmpdir"), fileName);
+
+            try(FileWriter writer = new FileWriter(file, true)) {
+                writer.write(content + "\n");
+            }
+            // S3에 업로드 (덮어쓰기 형식)
+            String s3Path = "scripts/" + fileName;
+            s3Service.uploadFileToS3(s3Path, file);
+
+            return null;
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "스크립트 저장 중 오류가 발생하였습니다.");
+        }
+    }
 }
