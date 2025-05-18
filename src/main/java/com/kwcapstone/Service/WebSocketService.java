5cd8aed96a332e4802a51f3f617b43724738226c
seoyeon0103 <angel6462@naver.com>
2025-05-18 19:07:22 +0900

    Feat: Summary Service 구현

--- src/main/java/com/kwcapstone/Service/WebSocketService.java
+++ src/main/java/com/kwcapstone/Service/WebSocketService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -22,21 +23,35 @@ public class WebSocketService {
     //concurrentHashMap -> ,여러 스레득 도시에 접근해도 안전하게 작동
     //내부적으로 데이터를 나눠서(lock 분할) 처리해서 성능도 높고, 충돌도 방지
     private final Map<String, List<String>> scriptBuffer = new ConcurrentHashMap<>();
+    private final Map<String, Integer> newScriptionCounter = new ConcurrentHashMap<>();
 
     //실시간 회의 스크립트를 websocket으로 받아서 GPT로 요약하고 이 요약한 것을 webSocket을 다시 클라이언트들에게 전달하는 핵심 로직
     public void handleScript(String projectId, ScriptMessageRequestDto dto) {
-        scriptBuffer.computeIfAbsent(projectId, k -> new ArrayList<>()).add(dto.getContent());
+        //scriptBuffer는 Map<Project, 스크립트 리스트> 구조
+        //스크립트에 새로운 문장 추가
+        scriptBuffer.computeIfAbsent(projectId, k -> new ArrayList<>()).add(dto.getScription());
 
+        //projctId를 통해 현재 회의바에 저장딘 전체 스크립트 리스트를 가져옴
         List<String> scriptList = scriptBuffer.get(projectId);
-        if (scriptList.size() >= 7) {
+
+        // 신규 문장 카운트 증가
+        int currentCount = newScriptionCounter.getOrDefault(projectId, 0) + 1;
+        newScriptionCounter.put(projectId, currentCount);
+
+        //새로운 문장이 7개 ㅇ상이면 gpt 호출
+        if (currentCount >=  7) {
             String fullText = String.join(" ", scriptList);
-            String summary = gptService.summarizeText(fullText);
+            String summary = gptService.callSummaryOpenAI(fullText);
 
             // 요약본 WebSocket으로 전송
-            messagingTemplate.convertAndSend("/topic/summary/" + projectId, summary);
+            simpMessagingTemplate.convertAndSend("/topic/summary/" + projectId,
+                    Map.of(
+                            "event", "summary",
+                            "projectId", projectId,
+                            "summary", summary
+                    ));
 
-            // 버퍼 초기화 or 일부만 남기기 (ex. 최근 3개는 유지 등)
-            scriptBuffer.put(projectId, new ArrayList<>(scriptList.subList(scriptList.size() - 3, scriptList.size())));
+            newScriptionCounter.put(projectId, 0);
         }
     }
 }
