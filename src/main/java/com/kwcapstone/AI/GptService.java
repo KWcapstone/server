fc17c4d30c11b7f412a793d1de618b2f716d2463
seoyeon0103 <angel6462@naver.com>
2025-05-19 16:28:07 +0900

    Feat: 없어졌던 main keyword 추천하기 기능 다시 축

--- src/main/java/com/kwcapstone/AI/GptService.java
+++ src/main/java/com/kwcapstone/AI/GptService.java
@@ -64,8 +64,40 @@ public class GptService {
                 .block(); // block은 동기식으로 기다리기 (필요 시 비동기 방식으로 분리 가능)
     }
 
-//    public String callRecommendedKeywords(String prompt) {
-//        // int maxTokens =
-//        String promptMessage = "";
-//    }
+    //gpt에게 요청하는 call를 만들어줘야할 것 같음
+    // open ai 는 max_tokens 생성할 때 최대 토큰 수를 으미함
+    // prompt 가 응답 모두 토큰으로 계산되어 얼마나 길게 나올 수 있는지를 제한함
+    //prompt 는 누적된 회의 스크립트 전체 텍스트
+    public String callMainOpenAI(String prompt) {
+        int maxTokens = estimateMaxTokens(prompt);
+
+        String promptMessage = """
+            다음 텍스트는 현재 진행 중인 아이디에이션 과정의 스크립트야. 이의 주제를 파악하고 현재 아이디에이션에 대한 주요 키워드를 5개 알려줘.
+            주요 키워드만 대답해주면 돼. "더 필요하신거 있으신가요" 과 같은 답변 이어서 하지마. JSON 배열로 추출해줘.
+            """.formatted(maxTokens);
+
+        Map<String, Object> requestBody = Map.of(
+                "model", gptConfig.getModel(),
+                "messages", List.of(
+                        Map.of("role", "system", "content", promptMessage),
+                        Map.of("role", "user", "content", prompt) //원문 내용
+                ),
+                "temperature", 0.3, //창의성(무작위성)을 조절하는 파라미터
+                "max_tokens", maxTokens
+        );
+
+        // block()을 써서 동기적으로 받기
+        return openAiWebClient.post()
+                .uri("/chat/completions") // gpt 한테 보내는 엔드포인트
+                .bodyValue(requestBody)  //요청 본문 담을 데이터
+                .retrieve() // 응답을 받아오는 단계 -> 비동기 응답 객체로 흐름이 바뀜
+                .bodyToMono(Map.class)  // JSON으로 받음
+                .map(response -> {
+                    List<Map<String, Object>> choices = (List<Map<String, Object>>) response.get("choices");
+                    Map<String, Object> message = (Map<String, Object>) choices.get(0).get("message");
+                    return message.get("content").toString().trim();
+                })
+                .onErrorResume(e -> Mono.just("Error: " + e.getMessage()))
+                .block(); // block은 동기식으로 기다리기 (필요 시 비동기 방식으로 분리 가능)
+    }
 }
