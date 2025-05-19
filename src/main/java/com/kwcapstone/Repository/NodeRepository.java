b57785019ef668777a8fe284729661a2656f7639
seoyeon0103 <angel6462@naver.com>
2025-05-20 02:37:27 +0900

    Refactor: pw db 고치기

--- src/main/java/com/kwcapstone/Repository/NodeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kwcapstone.Repository;
-
-import com.kwcapstone.Domain.Entity.Node;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface NodeRepository extends MongoRepository<Node, ObjectId> {
-
-}
