6e99654bf2d3c04ce77ad44d8c34d1fc853013f1
seoyeon0103 <angel6462@naver.com>
2025-07-12 17:06:26 +0900

    Refactor: 프론트 임시 로컬 주소 추가

--- src/main/java/com/kwcapstone/Config/SecurityConfig.java
+++ src/main/java/com/kwcapstone/Config/SecurityConfig.java
@@ -56,6 +56,7 @@ public class SecurityConfig {
                 "https://moaba.site",
                 "http://3.39.11.168:8080", "https://3.39.11.168:8080",
                 "http://localhost:3000", "https://localhost:3000",
+                "http://localhost:80", "http://localhost:3000:80",
                 "http://43.201.65.48", "https://jiangxy.github.io" // 테스트 툴이라 나중엔 지워야
         ));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
