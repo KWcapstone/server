79279fa8aafea7b94eb2ab6a6ee6dd6eb9828e50
seoyeon0103 <angel6462@naver.com>
2025-07-17 21:01:57 +0900

    Feat: RedisConfig 추가

--- src/main/java/com/kwcapstone/Config/RedisConfig.java
+++ src/main/java/com/kwcapstone/Config/RedisConfig.java
@@ -1,10 +1,15 @@
 package com.kwcapstone.Config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.data.redis.RedisConnectionDetails;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 @RequiredArgsConstructor
@@ -12,7 +17,22 @@ public class RedisConfig {
     private final RedisProperties properties;
 
     @Bean
-    public RedisConnectionDetails redisConnectionDetails() {
-        RedisConfigu
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisConfiguration config = new RedisStandaloneConfiguration(properties.getHost(), properties.getPort());
+        LettuceClientConfiguration.LettuceClientConfigurationBuilder clientConfigurationBuilder = LettuceClientConfiguration.builder();
+
+        if(Boolean.TRUE.equals(properties.getSsl().isEnabled())){
+            clientConfigurationBuilder.useSsl();
+        }
+
+        return new LettuceConnectionFactory(config, clientConfigurationBuilder.build());
+
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return  redisTemplate;
     }
 }
